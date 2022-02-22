d5e0f7e7b424738615830011ac30703216847be5
Dan Forder <danfrdr@gmail.com>
2022-02-22 16:45:25 +0000

    tech test example

---
+++ script.js
@@ -0,0 +1,44 @@
+import pokemonArray from "./data/pokemon.js";
+
+const cardContainer = document.querySelector(".card-container");
+
+const capitaliseFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const getTypesText = (types) => {
+  if (types.length === 1) {
+    return types[0];
+  }
+
+  if (types.length === 2) {
+    return `${types[0]} & ${types[1]}`;
+  }
+
+  console.error("unhandled type array");
+};
+
+const createPokemonCards = () => {
+  cardContainer.innerHTML = "";
+
+  pokemonArray.forEach((pokemon) => {
+    const capitalisedName = capitaliseFirstLetter(pokemon.name);
+    const typesText = getTypesText(pokemon.types);
+
+    cardContainer.innerHTML += `
+	<div class="card">
+		<img
+			class="card__image"
+			src="${pokemon.sprite}"
+		/>
+		<div class="card__content">
+			<h2 class="card__heading">${capitalisedName}</h2>
+			<p class="card__text">
+				${capitalisedName} (#${pokemon.id}) is a ${typesText} type pokemon.
+			</p>
+		</div>
+	</div>`;
+  });
+};
+
+createPokemonCards();
